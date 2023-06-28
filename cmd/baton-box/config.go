25022d6a46c0bd0e187be829e306ddcd14512b8f
Justin Gallardo <justin.gallardo@conductorone.com>
2023-06-28 16:05:53 -0700

    Rename client-id/secret

--- cmd/baton-box/config.go
+++ cmd/baton-box/config.go
@@ -12,18 +12,18 @@ import (
 type config struct {
 	cli.BaseConfig `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
 
-	ClientID     string `mapstructure:"client-id"`
-	ClientSecret string `mapstructure:"client-secret"`
+	ClientID     string `mapstructure:"box-client-id"`
+	ClientSecret string `mapstructure:"box-client-secret"`
 	EnterpriseID string `mapstructure:"enterprise-id"`
 }
 
 // validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
 func validateConfig(ctx context.Context, cfg *config) error {
 	if cfg.ClientID == "" {
-		return fmt.Errorf("client id is missing")
+		return fmt.Errorf("box client id is missing")
 	}
 	if cfg.ClientSecret == "" {
-		return fmt.Errorf("client secret is missing")
+		return fmt.Errorf("box client secret is missing")
 	}
 	if cfg.EnterpriseID == "" {
 		return fmt.Errorf("enterprise id is missing")
@@ -33,7 +33,7 @@ func validateConfig(ctx context.Context, cfg *config) error {
 
 // cmdFlags sets the cmdFlags required for the connector.
 func cmdFlags(cmd *cobra.Command) {
-	cmd.PersistentFlags().String("client-id", "", "Client ID used to authenticate to the Box API. ($BATON_CLIENT_ID)")
-	cmd.PersistentFlags().String("client-secret", "", "Client Secret used to authenticate to the Box API. ($BATON_CLIENT_SECRET)")
+	cmd.PersistentFlags().String("box-client-id", "", "Client ID used to authenticate to the Box API. ($BATON_BOX)_CLIENT_ID)")
+	cmd.PersistentFlags().String("box-client-secret", "", "Client Secret used to authenticate to the Box API. ($BATON_BOX_CLIENT_SECRET)")
 	cmd.PersistentFlags().String("enterprise-id", "", "ID of your Box enterprise. ($BATON_ENTERPRISE_ID)")
 }
