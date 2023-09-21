982bc2c97f9906d181794d5cab6ec4e36301662c
Martin Albert <martin.albert.187@gmail.com>
2023-09-21 04:41:43 +0200

    Clean up (#4)
    * Update dependencies
    
    * Fix notice file typo
    
    * Skip ents and grants when syncing users
    
    * Update usage in readme

--- pkg/connector/helpers.go
+++ pkg/connector/helpers.go
@@ -3,10 +3,18 @@ package connector
 import (
 	"fmt"
 
+	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
+	"github.com/conductorone/baton-sdk/pkg/annotations"
 	ent "github.com/conductorone/baton-sdk/pkg/types/entitlement"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
+
+func annotationsForUserResourceType() annotations.Annotations {
+	annos := annotations.Annotations{}
+	annos.Update(&v2.SkipEntitlementsAndGrants{})
+	return annos
+}
 
 func titleCase(s string) string {
 	titleCaser := cases.Title(language.English)
