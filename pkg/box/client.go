53210ad8e834f909332ff2dc0493bff892f5149d
Bjorn <bjorn@ambientchill.com>
2025-03-07 12:59:40 -0800

    Return a helpful error message when login fails (#9)
    * Return a helpful error message when login fails
    
    * Add defer close before checking status
    
    * fix lint

--- pkg/box/client.go
+++ pkg/box/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -67,7 +68,6 @@ func RequestAccessToken(ctx context.Context, clientID string, clientSecret strin
 	if err != nil {
 		return "", err
 	}
-
 	authUrl := fmt.Sprint(baseUrl, "/oauth2/token")
 	data := url.Values{}
 	data.Add("client_id", clientID)
@@ -88,8 +88,15 @@ func RequestAccessToken(ctx context.Context, clientID string, clientSecret strin
 	if err != nil {
 		return "", err
 	}
-
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		return "", fmt.Errorf("failed to get access token: %s status: %s", string(body), resp.Status)
+	}
 
 	var res struct {
 		AccessToken string `json:"access_token"`
