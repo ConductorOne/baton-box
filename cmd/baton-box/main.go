8d435d759640546f081ab0bc2ff5424ee85ca4f6
Steve Gontzes <gontzess@gmail.com>
2026-01-27 16:45:18 -0500

    feat: upgrade SDK to v0.7.4 and Go to 1.25.2
    - Migrate from cli.NewCmd to config.DefineConfiguration pattern
    - Update CI workflow to use go-version-file from go.mod
    - Update .golangci.yml to v2 format with ruleguard support
    - Add tools/rules.go for ruleguard linting
    - Add Makefile generate target and Lambda build tags

--- cmd/baton-box/main.go
+++ cmd/baton-box/main.go
@@ -6,8 +6,10 @@ import (
 	"os"
 
 	"github.com/conductorone/baton-box/pkg/connector"
-	"github.com/conductorone/baton-sdk/pkg/cli"
+	cfg "github.com/conductorone/baton-box/pkg/config"
+	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
@@ -18,15 +20,19 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 
-	cfg := &config{}
-	cmd, err := cli.NewCmd(ctx, "baton-box", cfg, validateConfig, getConnector)
+	_, cmd, err := config.DefineConfiguration(
+		ctx,
+		"baton-box",
+		getConnector,
+		cfg.Config,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Box{}),
+	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
 	cmd.Version = version
-	cmdFlags(cmd)
 
 	err = cmd.Execute()
 	if err != nil {
@@ -35,20 +41,20 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, c *cfg.Box) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	c, err := connector.New(ctx, cfg.ClientID, cfg.ClientSecret, cfg.EnterpriseID)
+	cb, err := connector.New(ctx, c.BoxClientId, c.BoxClientSecret, c.EnterpriseId)
 	if err != nil {
 		l.Error("error creating box connector", zap.Error(err))
 		return nil, err
 	}
 
-	connector, err := connectorbuilder.NewConnector(ctx, c)
+	conn, err := connectorbuilder.NewConnector(ctx, cb)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
 
-	return connector, nil
+	return conn, nil
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"github.com/conductorone/baton-sdk/pkg/field"
+)
+
+var (
+	ClientID = field.StringField(
+		"box-client-id",
+		field.WithDescription("Client ID used to authenticate to the Box API."),
+		field.WithRequired(true),
+	)
+
+	ClientSecret = field.StringField(
+		"box-client-secret",
+		field.WithDescription("Client Secret used to authenticate to the Box API."),
+		field.WithRequired(true),
+		field.WithIsSecret(true),
+	)
+
+	EnterpriseID = field.StringField(
+		"enterprise-id",
+		field.WithDescription("ID of your Box enterprise."),
+		field.WithRequired(true),
+	)
+)
+
+//go:generate go run ./gen
+var Config = field.NewConfiguration([]field.SchemaField{
+	ClientID,
+	ClientSecret,
+	EnterpriseID,
+})
